c19c2ec9cbd61afbc5a86931772805083f473dbb
Alex Makarov <aleksejmakarov432@gmail.com>
2025-02-27 22:58:43 +0300

    Create task permissions enum Add marshmallow and rest_marshmallow

--- todo_core/common/choices/__init__.py
+++ todo_core/common/choices/__init__.py
@@ -1,2 +1,3 @@
 from .project_permissions import ProjectPermissions
 from .roles import Roles
+from .task_permissions import TaskPermissions
---
+++ todo_core/common/choices/task_permissions.py
@@ -0,0 +1,6 @@
+from enum import StrEnum
+
+
+class TaskPermissions(StrEnum):
+    create = "create_tasks"
+    get = "get_tasks"
