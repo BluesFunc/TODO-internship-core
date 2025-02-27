23942beaa335804843e3eab0d5d351a879a248fb
Alex Makarov <aleksejmakarov432@gmail.com>
2025-02-27 14:32:54 +0300

    Add delete function in serializer

--- todo_core/tasks/services/task_status_subscriber_service.py
+++ todo_core/tasks/services/task_status_subscriber_service.py
@@ -1,4 +1,6 @@
-from tasks.models import TaskStatusSubscribers
+from uuid import UUID
+
+from tasks.models import Task, TaskStatusSubscribers
 
 
 class TaskStatusSubscriberService:
@@ -7,3 +9,7 @@ class TaskStatusSubscriberService:
     def create(model: TaskStatusSubscribers) -> TaskStatusSubscribers:
         model.save()
         return model
+
+    @staticmethod
+    def delete(task: Task, user_id: UUID) -> None:
+        TaskStatusSubscribers.objects.get(task_id=task, user_id=user_id).delete()
